70e519e6b96dfeb64f3af561967cecc655730f57
Erick <erickfernandess2@gmail.com>
2023-11-21 15:11:45 -0500

    Creating React app with Vite

---
+++ app/src/components/Banner/index.jsx
@@ -0,0 +1,11 @@
+import bgHeaderDesktop from '../../assets/images/bg-header-desktop.svg'
+
+const Banner = () => {
+    return(
+        <header>
+            <img src={ bgHeaderDesktop } alt="Banner" />
+        </header>
+    )
+}
+
+export default Banner
